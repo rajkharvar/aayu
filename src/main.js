69905023d17cd05d8b6cd44450f91f61b1cb09df
rajkharvar <rajkharvar1808@gmail.com>
2020-02-22 12:00:20 +0530

    lazy commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueMq from 'vue-mq'
 import { Datetime } from 'vue-datetime'
 
 import 'vue-datetime/dist/vue-datetime.css'
@@ -9,7 +8,6 @@ import './assets/scss/style.scss'
 
 Vue.config.productionTip = false
 
-Vue.use(VueMq)
 Vue.use(Datetime)
 
 new Vue({
